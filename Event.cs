45fa52984414e388394194f40ba135fdec70024e
Jacob Bateman <bat18004@byui.edu>
2026-07-14 13:33:20 -0600

    Added classes for event, lectures, receptions and outdoor gatherings. Made the classes lectures, receptions and outdoor gatherings inherit from the event class.

---
+++ Event.cs
@@ -0,0 +1,8 @@
+protected class Event
+{
+    string EventTitle = "";
+    string Description = "";
+    int Date = "";
+    int Time = "";
+    string Address = "";
+}
---
+++ Outdoor-Gatherings.cs
@@ -0,0 +1,2 @@
+private class OutdoorGatherings:Event
+{}
